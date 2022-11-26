f081d5c8ab3e08b904176c4a0214e89856eb6714
Maxim <makfedorovtooo@gmail.com>
2022-11-26 13:58:20 +0300

    Add description

--- lesson_2.py
+++ lesson_2.py
@@ -1,3 +1,4 @@
+## Задача 1
 turist = 90
 det_know = 30
 en_know = 28
@@ -10,10 +11,7 @@ v_out_all_det_fr = det_fr - all_lang
 v_out_all_en_det = en_and_det - all_lang
 v_out_all_en_fr = en_and_fr - all_lang
 only_det = det_know - (v_out_all_det_fr + v_out_all_en_det + all_lang)
-print(only_det)
 only_en = en_know - (v_out_all_en_det + v_out_all_en_fr + all_lang)
-print(only_en)
 only_fr = fr_know - (v_out_all_en_fr + v_out_all_det_fr + all_lang)
-print(only_fr)
 no_foring_lang = turist - (only_det + only_en + only_fr + all_lang + v_out_all_det_fr +v_out_all_en_det + v_out_all_en_fr)
-print(no_foring_lang)
+print(f'Туристов не говорящих на иностранных языках {no_foring_lang}')
